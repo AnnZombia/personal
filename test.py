74ebcc2c4fc238df83c398e46e29bbf7eb21da45
AnnZombia <drugaleva@mail.ru>
2022-02-09 10:46:59 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,29 +3,36 @@ from flask import request, Flask
 from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 
-mydb = mysql.connector.connect(
-     host = "localhost",
-     user = "root",
-     password = "Aksenov/1",
-     database = "app"
-     )
-
 @app.route('/auth_phone', methods=['POST'])
 def auth_phone():
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Aksenov/1",
+        database = "app"
+        )
     phone = request.args.get('phone')
     cursor = mydb.cursor()
     cursor.execute(''' INSERT INTO auth (phone) VALUES (%s) ''', phone)
     mydb.commit()
     cursor.close()
+    mydb.close()
 
 @app.route('/auth_code', methods=['POST'])
 def auth_code():
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Aksenov/1",
+        database = "app"
+        )
     code = request.args.get('code')
     phone = request.args.get('phone')
     cursor = mysql.connection.cursor()
     cursor.execute(''' INSERT INTO auth (code) WHERE phone = %s VALUES (%s)''', (phone, code))
     mysql.connection.commit()
     cursor.close()
+    mydb.close()
 
 if __name__ == '__main__':
 	app.run(port=1234,host='0.0.0.0')
