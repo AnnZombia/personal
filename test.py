3d76bb4dfffc9b56f6a7851c33a8b12419951057
annzombia <drugaleva@mail.ru>
2022-02-09 07:45:03 +0000

    09.02

--- test.py
+++ test.py
@@ -1,17 +1,31 @@
-from flask import Flask
+from flaskext.mysql import MySQL
+from flask import request, Flask
 from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
-api = Api(app)
 
-@app.route('/auth', methods=['POST'])
+mydb = mysql.connector.connect(
+     host = "localhost",
+     user = "root",
+     password = "Aksenov/1",
+     database = "app"
+     )
+
+@app.route('/auth_phone', methods=['POST'])
 def auth_phone():
-    global auth_phone
-    auth_phone = request.args.get('phone', type=int)
+    phone = request.args.get('phone')
+    cursor = mydb.cursor()
+    cursor.execute(''' INSERT INTO auth (phone) VALUES (%s) ''', phone)
+    mydb.commit()
+    cursor.close()
 
-@app.route('/code', methods=['POST'])
+@app.route('/auth_code', methods=['POST'])
 def auth_code():
-    code = request.args.get('code', type=int)
-    return code
-	
+    code = request.args.get('code')
+    phone = request.args.get('phone')
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' INSERT INTO auth (code) WHERE phone = %s VALUES (%s)''', (phone, code))
+    mysql.connection.commit()
+    cursor.close()
+
 if __name__ == '__main__':
 	app.run(port=1234,host='0.0.0.0')
