37a57ae1557f7cfd169f5af24dde758bf7a36150
AnnZombia <drugaleva@mail.ru>
2022-02-16 13:57:00 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,43 @@
-import api_auth as api_auth
+import telebot
+import mysql.connector
+from telethon.sync import TelegramClient
+from telethon.tl.types import InputPeerUser, InputPeerChannel
+from telethon import TelegramClient, sync, events
+import auth_getphone as auth_getphone
 from random import random, randint
 
-uniq_key = randint (1000000000,9999999999)
-api_auth.main(uniq_key)
+def main():
+  api_id = 10787535
+  api_hash = 'f4c93d55681e17b14d516e8f5571e4cd'
+  username = 'AnnZombia'
+  uniq_key = randint (1000000000,9999999999)
+  
+  client = TelegramClient(username, api_id, api_hash)
+  client.connect()
+
+  auth_getphone.main(uniq_key)
+  mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Aksenov/1",
+    database = "app"
+    )
+  
+  if not client.is_user_authorized():
+    cursor = mydb.cursor()
+    auth_getphone.main(uniq_key)
+    phone = cursor.execute("SELECT phone FROM auth WHERE uniq = %s", (uniq_key,))
+    client.send_code_request(phone)
+    auth_code.main()
+    code = cursor.execute("SELECT code FROM auth WHERE uniq = %s", (uniq_key,))
+    mydb.commit()
+    cursor.close()
+    mydb.close()
+    
+    try:
+        client.sign_in(phone, code))
+    except SessionPasswordNeededError:
+        client.sign_in(password))
+    
+    me = client.get_me()
+    print(me)
