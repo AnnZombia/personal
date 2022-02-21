a47e391154d48b946848622fc715fcf1a563fb69
AnnZombia <drugaleva@mail.ru>
2022-02-21 16:56:46 +0300

    Update auth_getphone.py

--- auth_getphone.py
+++ auth_getphone.py
@@ -1,12 +1,10 @@
 import mysql.connector
-import os
-import multiprocessing
-from multiprocessing import Process, Event
+from multiprocessing import Process
 from flask import request, Flask
 from flask_restful import Api, Resource, reqparse
+
 app = Flask(__name__)
 app.debug = False
-#event = multiprocessing.Event()
 
 @app.route('/auth_phone', methods=['POST'])
 def auth_phone():
@@ -17,28 +15,63 @@ def auth_phone():
         database = "app"
         )
     parser = reqparse.RequestParser()
+    parser.add_argument("uniq_key")
     parser.add_argument("phone")
+    parser.add_argument("name")
     params = parser.parse_args()
+    uniq_key = int(params["uniq_key"]) 
     phone = int(params["phone"])
+    name = params["name"]
     cursor = mydb.cursor()
-    cursor.execute("INSERT INTO auth (phone, uniq) VALUES (%s, %s)", (phone, uniq_key))
+    
+    # не забыть сделать проверку ключа на уникальность
+    
+    if name is None:
+        cursor.execute("INSERT INTO auth (phone, uniq) VALUES (%s, %s)", (phone, uniq_key))
+    elif phone is None:
+        cursor.execute("INSERT INTO auth (name, uniq) VALUES (%s, %s)", (name, uniq_key))
+    elif name != None and phone != None:
+        cursor.execute("INSERT INTO auth (name, phone, uniq) VALUES (%s, %s, %s)", (name, phone, uniq_key))
+        
     mydb.commit()
     cursor.close()
     mydb.close()
-#    event.clear()
     return "200"
+
+@app.route('/auth_code', methods=['POST'])
+def auth_code():
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Aksenov/1",
+        database = "app"
+        )
+    parser = reqparse.RequestParser()
+    parser.add_argument("name")
+    parser.add_argument("phone")
+    parser.add_argument("code")
+    params = parser.parse_args()
+    phone = int(params["phone"])
+    code = int(params["code"])
+    name = params["name"]
+    cursor = mydb.cursor()
+    
+    if name is None:
+        cursor.execute("UPDATE auth SET code=%s WHERE uniq = %s" and phone=%s, (code, uniq_key, phone))
+    elif phone is None:
+        cursor.execute("UPDATE auth SET code=%s WHERE uniq = %s" and name=%s, (code, uniq_key, name))
+    elif name != None and phone != None:
+        cursor.execute("UPDATE auth SET code=%s WHERE uniq = %s" and phone=%s and name=%s, (code, uniq_key, phone, name))
+    
+    mydb.commit()
+    cursor.close()
+    mydb.close()
+    return "200"
+
 
 def api():
     app.run(port=1234,host='0.0.0.0')
 
-def main(key):
-    global uniq_key 
-    uniq_key = key
-#    event.set()
+def main():
     multi = multiprocessing.Process(target=api)
     multi.start()
-#    while True:
-#        if event.is_set() != True:
-#            multi.terminate()
-#            multi.join()
-#            break
