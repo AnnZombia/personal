27955c014429810ad7698a325dc20d3d6010d92f
AnnZombia <drugaleva@mail.ru>
2022-02-22 17:45:22 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -108,7 +108,8 @@ def auth_code():
     client = TelegramClient(str(uniq_key), api_id, api_hash, loop=loop) 
     client.connect()
     try:
-      client.sign_in(phone, code)
+      #client.sign_in(phone, code)
+      me = client.sign_in(phone_number, input('Enter code: '))
       return "200"
     except SessionPasswordNeededError:
       client.sign_in(password)
