2655b23977482464293d2aacae14dcb7e88b00ba
AnnZombia <drugaleva@mail.ru>
2022-02-24 17:28:16 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -9,53 +9,55 @@ import multiprocessing
 from flask import request, Flask
 from flask_restful import Api, Resource, reqparse
 
+status = {}
 app = Flask(__name__)
 app.debug = False
 api_id = 10787535
 api_hash = 'f4c93d55681e17b14d516e8f5571e4cd'
 
 def main():
-    global client
     multi = multiprocessing.Process(target=api)
     multi.start()
  
 # первоначальная проверка ключа на уникальность
 @app.route('/auth_init', methods=['POST'])
 def auth_init():
-  parser = reqparse.RequestParser()
-  parser.add_argument("uniq_key")
-  params = parser.parse_args()
-  uniq_key = int(params["uniq_key"]) 
+    parser = reqparse.RequestParser()
+    parser.add_argument("uniq_key")
+    params = parser.parse_args()
+    uniq_key = int(params["uniq_key"]) 
   
-  mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "Aksenov/1",
-    database = "app",
-    get_warnings = True
-    )
-  cursor = mydb.cursor()
-  cursor.execute("SELECT * FROM auth WHERE uniq = %s", (uniq_key,))
-  record = cursor.fetchone()
-  mydb.commit()
-  cursor.close()
-  mydb.close()
-  if record == None:
-    return "200"
-  else:
-    return "500"
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Aksenov/1",
+        database = "app",
+        get_warnings = True
+        )
+    
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM auth WHERE uniq = %s", (uniq_key,))
+    record = cursor.fetchone()
+    mydb.commit()
+    cursor.close()
+    mydb.close()
+    if record == None:
+        return "200"
+    else:
+        return "500"
 
 # начало регистрации
 @app.route('/auth_phone', methods=['POST'])
 def auth_phone():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+#    loop = asyncio.new_event_loop()
+#    asyncio.set_event_loop(loop)
+    
     mydb = mysql.connector.connect(
-       host = "localhost",
-       user = "root",
-       password = "Aksenov/1",
-       database = "app"
-       )
+        host = "localhost",
+        user = "root",
+        password = "Aksenov/1",
+        database = "app"
+        )
     parser = reqparse.RequestParser()
     parser.add_argument("uniq_key")
     parser.add_argument("phone")
@@ -66,16 +68,15 @@ def auth_phone():
     phone = int(params["phone"])
     name = params["name"]
     password = params["password"]
-    client = TelegramClient(str(uniq_key), api_id, api_hash, loop=loop) 
-    client.connect()
-    client.send_code_request('+'+(params["phone"]))
-    client.disconnect() 
     cursor = mydb.cursor()
     cursor.execute("INSERT INTO auth (name, phone, uniq, password) VALUES (%s, %s, %s, %s)", (name, phone, uniq_key, password))
     mydb.commit()
     cursor.close()
     mydb.close()
-    
+
+    status = {phone:0}
+    multi = multiprocessing.Process(target=login, args=(uniq_key, phone, password)
+    multi.start()
     return "200"
 
 # получаем код и выполняем вход
@@ -98,25 +99,47 @@ def auth_code():
     cursor = mydb.cursor(buffered=True)
     cursor.execute("UPDATE auth SET code=%s WHERE uniq = %s", (code, uniq_key))
     mydb.commit()
-    cursor.execute("SELECT phone, password, hash FROM auth WHERE uniq = %s", (uniq_key,))
+    cursor.execute("SELECT phone FROM auth WHERE uniq = %s", (uniq_key,))
     mydb.commit()
     record = cursor.fetchone()
     cursor.close()
     mydb.close()
     phone = '+'+str(record[0])
-    password = str(record[1])
-    client = TelegramClient(str(uniq_key), api_id, api_hash, loop=loop) 
-    client.connect()
-    client.send_code_request(phone)
-    try:
-      client.sign_in(phone, code)
-      return "200"
-    except SessionPasswordNeededError:
-      client.sign_in(password)
-      return "200"
-    client.disconnect() 
-    
+    status.update({phone:1})
+                                    
+def login(uniq, phone_num, passw):
+    uniq_key = uniq
+    phone = phone_num
+    password = passw
 
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Aksenov/1",
+        database = "app"
+        )
+                    
+    client = TelegramClient(str(uniq_key), api_id, api_hash) 
+    client.connect()
+    client.send_code_request('+'+(params["phone"]))
+    while status[phone] == 1
+        pass
+                                    
+    cursor = mydb.cursor(buffered=True)
+    cursor.execute("SELECT code FROM auth WHERE uniq = %s", (uniq_key,))
+    record = cursor.fetchone()
+    cursor.close()
+    mydb.close()
+                                    
+    try:
+        client.sign_in(phone, str(record[0]))
+        return "200"
+    except SessionPasswordNeededError:
+        client.sign_in(password)
+        return "200"
+    client.disconnect() 
+    del status[phone]
+    
 def api():
     app.run(port=1234,host='0.0.0.0')
     
