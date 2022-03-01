ac33eb25d27020fe50ad241637d1357159dfdc9c
AnnZombia <drugaleva@mail.ru>
2022-03-01 12:13:16 +0300

    Update user_edit.py

--- user_edit.py
+++ user_edit.py
@@ -27,7 +27,7 @@ def get_user():
     parser.add_argument("goal")
     params = parser.parse_args()
     uniq_key = int(params["uniq_key"])
-    if params["phone"]) != None:
+    if params["phone"] != None:
         prey_phone = int(params["phone"])
     if params["name"] != None:
         prey_name = params["name"]
