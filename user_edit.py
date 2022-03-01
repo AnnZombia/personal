b8401f0b93917bbd976974feca517a81da50a94b
AnnZombia <drugaleva@mail.ru>
2022-03-01 16:43:37 +0300

    Update user_edit.py

--- user_edit.py
+++ user_edit.py
@@ -8,14 +8,8 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 app.debug = False
 
-@app.route('/get_user', methods=['POST'])
-def get_user():
-    mydb = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        password = "Aksenov/1",
-        database = "app"
-        )
+@app.route('/del_user', methods=['POST'])
+def del_user():
     parser = reqparse.RequestParser()
     parser.add_argument("uniq_key")
     parser.add_argument("name")
@@ -28,16 +22,43 @@ def get_user():
     goal = params["goal"] 
     cursor = mydb.cursor()
     
-    # adding/deleting prey
-    if goal != 'delete':
-        cursor.execute("SELECT * FROM queries WHERE uniq=%s and name=%s and phone=%s and goal=%s",  (uniq_key, name, phone, goal))
-        record = cursor.fetchone()
-        if record == None:
-            print("V BAZE:"+str(record))
-            cursor.execute("INSERT INTO queries (uniq, name, phone, goal, time) VALUES (%s, %s, %s, %s, %s)", (uniq_key, name, phone, goal, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-    else:
-        cursor.execute("DELETE FROM queries WHERE uniq=%s and phone=%s and name=%s", (uniq_key, phone, name))
-                    
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Aksenov/1",
+        database = "app"
+        )    
+    cursor.execute("DELETE FROM queries WHERE uniq=%s and phone=%s and name=%s", (uniq_key, phone, name))       
+    mydb.commit()
+    cursor.close()
+    mydb.close()
+    return "200"
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    parser = reqparse.RequestParser()
+    parser.add_argument("uniq_key")
+    parser.add_argument("name")
+    parser.add_argument("phone")
+    parser.add_argument("goal")
+    params = parser.parse_args()
+    uniq_key = int(params["uniq_key"])
+    phone = params["phone"]
+    name = params["name"]
+    goal = params["goal"] 
+
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Aksenov/1",
+        database = "app"
+        )
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM queries WHERE uniq=%s and name=%s and phone=%s and goal=%s",  (uniq_key, name, phone, goal))
+    record = cursor.fetchone()
+    if record == None:
+        cursor.execute("INSERT INTO queries (uniq, name, phone, goal, time) VALUES (%s, %s, %s, %s, %s)", (uniq_key, name, phone, goal, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     mydb.commit()
     cursor.close()
     mydb.close()
