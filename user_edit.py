d65722c58d2377d951388b6a08da07dff48a1aa2
AnnZombia <drugaleva@mail.ru>
2022-03-01 15:57:19 +0300

    Update user_edit.py

--- user_edit.py
+++ user_edit.py
@@ -1,16 +1,12 @@
-import os
 import datetime
-import auth
 from telethon.tl.functions.users import GetFullUserRequest
-import subprocess
 import mysql.connector
-import multiprocessing
+import threading
 from multiprocessing import Process, Event
 from flask import request, Flask
 from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 app.debug = False
-event = multiprocessing.Event()
 
 @app.route('/get_user', methods=['POST'])
 def get_user():
@@ -34,13 +30,6 @@ def get_user():
     goal = params["goal"] 
     cursor = mydb.cursor()
     
-    # check registration
-    cursor.execute("SELECT register FROM auth WHERE uniq = %s", (uniq_key,))
-    record = cursor.fetchone()
-    if record is False:
-        auth.main()
-    mydb.commit()
-    
     # adding/deleting prey
     if goal != 'delete':
         cursor.execute("INSERT INTO queries (uniq, prey_name, prey_phone, goal, time) VALUES (%s, %s, %s, %s, %s)", (uniq_key, prey_name, prey_phone, goal, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
@@ -53,21 +42,11 @@ def get_user():
     event.clear()
     return "200"
 
-@app.route('/deauth', methods=['POST'])
-def deauth():
-    parser = reqparse.RequestParser()
-    parser.add_argument("uniq_key")
-    params = parser.parse_args()
-    uniq_key = params["uniq_key"] 
-    os.remove("./"+uniq_key+".session")
-    return "200"
-
-
 def api():
     app.run(port=1235,host='0.0.0.0')
 
 def main():
-    multi = multiprocessing.Process(target=api)
-    multi.start()
+    thread1 = threading.Thread(target=api)
+    thread1.start()
     
 main()
