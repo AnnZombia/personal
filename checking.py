b9afcb95aec61525d8a3adeca5141b6b396ab102
AnnZombia <drugaleva@mail.ru>
2022-03-10 18:04:41 +0300

    Update checking.py

--- checking.py
+++ checking.py
@@ -7,6 +7,7 @@ from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerUser, InputPeerChannel
 from telethon import TelegramClient, events, sync
 
+status = None
 api_id = 10787535
 api_hash = 'f4c93d55681e17b14d516e8f5571e4cd'
 
@@ -33,11 +34,22 @@ def main():
                 if full.user.status != None:
                     print(full)
                     cursor.execute("INSERT INTO blocked (uniq, name, phone, time) VALUES (%s, %s, %s, %s)", (record[i][0], record[i][1], record[i][2], datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-  #          if record[i][3] == 'status':
-  #              if full.user.status != :
-                        
-            
-            
+            if record[i][3] == 'status':
+                cursor.execute("SELECT * FROM status WHERE uniq=%s and name=%s and phone=%s",  (record[i][0], record[i][1], record[i][2]))
+                record = cursor.fetchall()
+                last_status = record[len(record)-1][3]
+                print(last_status)
+                
+            if isinstance(full.user.status, UserStatusOffline):
+                if last_status == None or last_status == 'Online':
+                    cursor.execute("INSERT INTO status (uniq, name, phone, status, time) VALUES (%s, %s, %s, %s, %s)", (record[i][0], record[i][1], 'Offline', record[i][2], datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+                elseif last_status == 'Offline':
+                    continue
+            if isinstance(full.user.status, UserStatusOnline):
+                if last_status == None or last_status == 'Offline':
+                    cursor.execute("INSERT INTO status (uniq, name, phone, status, time) VALUES (%s, %s, %s, %s, %s)", (record[i][0], record[i][1], 'Online', record[i][2], datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+                elseif last_status == 'Online':
+                    continue                           
             
             client.disconnect()
         break
