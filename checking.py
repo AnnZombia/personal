073b3e5b962fee3e1a2ce47f2e50156aececbfb2
AnnZombia <drugaleva@mail.ru>
2022-03-10 17:10:47 +0300

    Update checking.py

--- checking.py
+++ checking.py
@@ -23,24 +23,18 @@ def main():
         cursor.execute("SELECT * FROM queries")
         record = cursor.fetchall()
         for i in range(len(record)):
-            client = TelegramClient(str(record[i][1]), api_id, api_hash) 
+            client = TelegramClient(str(record[i][0]), api_id, api_hash) 
             try:
                  client.connect()
             except Exception as ex:
                  print(ex)
             if record[i][4] == 'block':
-                 full = client(GetFullUserRequest(record[i][2]))
+                 full = client(GetFullUserRequest(record[i][1]))
                  if full.user.status != None:
-                      cursor.execute("INSERT INTO blocked (uniq, name, phone, time) VALUES (%s, %s, %s, %s)", (record[i][1]), record[i][2]), record[i][3]), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-                       
+                      cursor.execute("INSERT INTO blocked (uniq, name, phone, time) VALUES (%s, %s, %s, %s)", (record[i][0]), record[i][1]), record[i][2]), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))                   
             client.disconnect()
         break
     
-    
-#    cursor.execute("SELECT * FROM queries WHERE uniq=%s and name=%s and phone=%s and goal=%s",  (uniq_key, name, phone, goal))
-#    record = cursor.fetchone()
-#    if record == None:
-#        cursor.execute("INSERT INTO queries (uniq, name, phone, goal, time) VALUES (%s, %s, %s, %s, %s)", (uniq_key, name, phone, goal, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     mydb.commit()
     cursor.close()
     mydb.close()
