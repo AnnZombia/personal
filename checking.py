9039965b46f872735928fe76d8ed0376122bcb17
AnnZombia <drugaleva@mail.ru>
2022-03-22 17:00:53 +0300

    Update checking.py

--- checking.py
+++ checking.py
@@ -90,7 +90,7 @@ def main():
             except Exception as ex:
                 print("DISCONNECT ERRROR")
                 print(ex)
-            time.sleep(3)    
+            time.sleep(15)    
         cursor.close()
         mydb.close()
         time.sleep(5)
