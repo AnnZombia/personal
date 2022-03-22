fd72ed16e06049f05f798bd55a933cd3e6b23218
AnnZombia <drugaleva@mail.ru>
2022-03-22 13:45:34 +0300

    Update checking.py

--- checking.py
+++ checking.py
@@ -43,7 +43,7 @@ def main():
                     if record[i][2] != 'None':
                         cursor.execute("DELETE FROM queries WHERE uniq=%s and name=%s and phone=%s and goal=%s",  (record[i][0], record[i][1], record[i][2], 'block'))
                     else:
-                        cursor.execute("DELETE FROM queries WHERE uniq=%s and name=%s and phone=%s and goal=%s",  (record[i][0], record[i][1], '', 'block'))
+                        cursor.execute("DELETE FROM queries WHERE uniq=%s and name=%s and and goal=%s",  (record[i][0], record[i][1], 'block'))
                     mydb.commit()
             if record[i][3] == 'status':
                 cursor.execute("SELECT * FROM status WHERE uniq=%s and name=%s and phone=%s",  (record[i][0], record[i][1], record[i][2]))
