b9ca63361cb80c57aa2cc733145a338b55f6348c
AnnZombia <drugaleva@mail.ru>
2022-03-23 16:27:22 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -6,6 +6,7 @@ from telethon.sync import TelegramClient
 from telethon.tl.types import InputPeerUser, InputPeerChannel
 import threading
 from flask import request, Flask
+inport telethon.errors
 from flask_restful import Api, Resource, reqparse
 
 status = {}
