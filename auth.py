e9ce7c33e4df636ccde6c3d98c68e8a13c62a277
AnnZombia <drugaleva@mail.ru>
2022-04-29 16:14:39 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -139,7 +139,6 @@ def login(uniq, phone_num, passw):
         print(ex)
     try:
  #       client.send_code_request('+'+str(phone))
-        print("sent")
     except Exception as ex:
         print(ex)
     while True:
