5d4e545c355241c1f438408038cc2f448ef1dd01
AnnZombia <drugaleva@mail.ru>
2022-05-26 14:58:02 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -1,5 +1,6 @@
 from yandex_music import Client
 client = Client().init()
 
-test =  client.usersLikesTracks('axl.and')
+test = client.tracks_list.TracksList(849346658).fetch_tracks()
+
 print(test)
