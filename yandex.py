8da1412b8ef32bff0be7b5b6105b9fa3b623f234
AnnZombia <drugaleva@mail.ru>
2022-05-31 14:11:08 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -1,7 +1,6 @@
 #from yandex_music import Client
 
-#TOKEN= 'AQAAAABh0mlaAAG8Xq0-0CDnOEUsiBThpsRaiqQ'
-#client = Client(TOKEN).init()
+
 #test = client.users_likes_tracks(kind=kind, user_id=863546497).fetch_tracks()
 #test = client.users_likes_tracks(user_id="annzombia")[0].fetch_track()
 #print(test)
@@ -11,5 +10,12 @@ from typing import TYPE_CHECKING, Optional
 from yandex_music import YandexMusicObject, Client, Account, Permissions, Subscription, Plus, StationData, Alert, Status
 from yandex_music.utils import model
 #status = Status('annzombia','read')
-perm = Permissions('annzombia')
+
+
+
+TOKEN= 'AQAAAABh0mlaAAG8Xq0-0CDnOEUsiBThpsRaiqQ'
+client = Client(TOKEN).init()
+
+
+perm = Permissions(client)
 print(perm)
