1dfb96b08ae9d50309f52cd70e81f6132b75f1e3
AnnZombia <drugaleva@mail.ru>
2022-05-31 15:36:33 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -1,5 +1,5 @@
 from yandex_music import Client
 
 client = Client().init()
-test = client.users_likes_tracks(user_id='annzombia')[0].fetch_track()
+test = client.users_likes_tracks('annzombia')[0].fetch_track()
 print(test)
