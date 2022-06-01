a91cd3baf7656491f1b15df2fc0be5cd8f336fcb
AnnZombia <drugaleva@mail.ru>
2022-06-01 14:24:37 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -1,10 +1,10 @@
 from yandex_music import Client
 
 uid='annzombia'
-TOKEN= 'AQAAAABh0mlaAAG8Xq0-0CDnOEUsiBThpsRaiqQ'
+#TOKEN= 'AQAAAABh0mlaAAG8Xq0-0CDnOEUsiBThpsRaiqQ'
 #TOKEN= 'AQAEA7qjeXezAAG8Xm_-oR7feUZrgdc05vNhcbc'
 #TOKEN= 'AQAAAABhqZ3-AAG8XgFAmcE-Jk7UhWuPQndiQIE'
-client = Client(TOKEN).init()
+client = Client().init()
 #test = client.users_likes_tracks(user_id=1641179482).fetch_tracks()
 test = client.users_likes_tracks(user_id=1638505982).fetch_tracks()
 #test = client.users_likes_tracks().fetch_tracks()
