fa4f063466c51a32107e7eec13e2f68db315b770
AnnZombia <drugaleva@mail.ru>
2022-06-01 14:25:56 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -1,14 +1,16 @@
 from yandex_music import Client
 
-uid='annzombia'
-#TOKEN= 'AQAAAABh0mlaAAG8Xq0-0CDnOEUsiBThpsRaiqQ'
-#TOKEN= 'AQAEA7qjeXezAAG8Xm_-oR7feUZrgdc05vNhcbc'
+
+TOKEN= 'AQAAAABh0mlaAAG8Xq0-0CDnOEUsiBThpsRaiqQ'
 #TOKEN= 'AQAAAABhqZ3-AAG8XgFAmcE-Jk7UhWuPQndiQIE'
-client = Client().init()
+
+client = Client(TOKEN).init()
+
 #test = client.users_likes_tracks(user_id=1641179482).fetch_tracks()
 test = client.users_likes_tracks(user_id=1638505982).fetch_tracks()
-#test = client.users_likes_tracks().fetch_tracks()
+
 print(test)
+
 #for i in range(len(test)):
 #      for j in range(len(test[i].artists)):
 #            print(test[i].artists[j]['name'],'-', test[i].title)
