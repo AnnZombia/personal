81b17db638e0068bc4a707540e49cf1ecb388a83
AnnZombia <drugaleva@mail.ru>
2022-06-01 14:06:26 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -4,7 +4,7 @@ uid='annzombia'
 TOKEN= 'AQAAAABh0mlaAAG8Xq0-0CDnOEUsiBThpsRaiqQ'
 #TOKEN= 'AQAAAABhqZ3-AAG8XgFAmcE-Jk7UhWuPQndiQIE'
 client = Client(token=TOKEN).init()
-test = client.users_likes_tracks(user_id=1641179482)
+test = client.users_likes_tracks(user_id=1641179482).fetch_tracks()
 #test = client.users_likes_tracks().fetch_tracks()
 print(test)
 #for i in range(len(test)):
