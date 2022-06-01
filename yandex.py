3b5f99e4b2b754452a791e2aea1361afd4f5e496
AnnZombia <drugaleva@mail.ru>
2022-06-01 14:37:00 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -9,10 +9,8 @@ client = Client(TOKEN).init()
 
 #test = client.users_likes_tracks(user_id=1641179482).fetch_tracks()
 #test = client.users_likes_tracks(user_id=1638505982).fetch_tracks()
-test = client.users_likes_tracks()
+test = client.users_likes_tracks(uid)
 
-print(test)
-
-#for i in range(len(test)):
-#      for j in range(len(test[i].artists)):
-#            print(test[i].artists[j]['name'],'-', test[i].title)
+for i in range(len(test)):
+      for j in range(len(test[i].artists)):
+            print(test[i].artists[j]['name'],'-', test[i].title)
