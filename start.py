c6956b4819dee02baff55188800733394d215d9b
AnnZombia <drugaleva@mail.ru>
2022-06-01 16:43:46 +0300

    Update start.py

--- start.py
+++ start.py
@@ -26,8 +26,8 @@ while True:
 
       for i in range(len(tracks)):
             for j in range(len(tracks[i].artists)):
-                  print(track)
                   track = tracks[i].artists[j]['name']+'-'+tracks[i].title
+                  print(track)
                   new_version.append(track)
             
       diff = list(set(last_version + new_version))
