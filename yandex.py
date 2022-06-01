4b3e69873775e21431d94865721713d29732439d
AnnZombia <drugaleva@mail.ru>
2022-06-01 14:17:05 +0300

    Update yandex.py

--- yandex.py
+++ yandex.py
@@ -1,8 +1,8 @@
 from yandex_music import Client
 
 uid='annzombia'
-#TOKEN= 'AQAAAABh0mlaAAG8Xq0-0CDnOEUsiBThpsRaiqQ'
-TOKEN= 'AQAEA7qjeXezAAG8Xm_-oR7feUZrgdc05vNhcbc'
+TOKEN= 'AQAAAABh0mlaAAG8Xq0-0CDnOEUsiBThpsRaiqQ'
+#TOKEN= 'AQAEA7qjeXezAAG8Xm_-oR7feUZrgdc05vNhcbc'
 #TOKEN= 'AQAAAABhqZ3-AAG8XgFAmcE-Jk7UhWuPQndiQIE'
 client = Client(TOKEN).init()
 #test = client.users_likes_tracks(user_id=1641179482).fetch_tracks()
